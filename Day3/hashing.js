8a8f8d46432c9f0f9497afc4b6e192b8afaca3cf
daxx5020 <dakshmakwana2146@gmail.com>
2024-05-27 23:03:47 +0530

    learned about character hashing

---
+++ Day3/hashing.js
@@ -0,0 +1,32 @@
+//Character hashing
+
+/*
+    How can we use hashing in characters?
+
+    So for that we have to thing called ASCII characters which are represented by a single byte (8 bits).
+
+    Example:-
+
+    lowercase a = 97;
+    lowercase z = 122;
+
+    Now how can we find any character from this ASCII?
+
+    if we represent f(102)
+
+    ASCII(f) - ASCII(a) = 102 - 91 = 5
+    Now what is 5 here.
+
+    a = 0;
+    b = 1;
+    c = 2;
+    d = 3;
+    e = 4;
+    f = 5;
+
+    so from this formula we faound num 5 and that represents f.
+
+    final formula = char - a
+
+*/
+
