8d580cdd0b79d81d1f1ecfed1cadac0a387f28b7
daxx5020 <dakshmakwana2146@gmail.com>
2024-06-06 01:04:09 +0530

    changes in day 12 code

--- Day 12/arrays.js
+++ Day 12/arrays.js
@@ -1,64 +1,64 @@
 //majority element
 
-// let arr = [1,1,2,2,2,2,3,3]
+let arr = [1,1,2,2,2,2,3,3]
 
-// const majorityElement = function (arr){
-//     let mpp = new Map();
-//     for (let i=0; i < arr.length; i++){
-//         if(mpp.has(arr[i])){
-//             mpp.set(arr[i],mpp.get(arr[i])+1)
-//         }
-//         else{
-//             mpp.set(arr[i],1)
-//         }
-//     }
+const majorityElement = function (arr){
+    let mpp = new Map();
+    for (let i=0; i < arr.length; i++){
+        if(mpp.has(arr[i])){
+            mpp.set(arr[i],mpp.get(arr[i])+1)
+        }
+        else{
+            mpp.set(arr[i],1)
+        }
+    }
     
-//     for (let [key,value] of mpp){
-//         if (value > Math.floor(arr.length/2)){
-//             return key;
-//         }
-//     }
-//     return -1;
-// }
+    for (let [key,value] of mpp){
+        if (value > Math.floor(arr.length/2)){
+            return key;
+        }
+    }
+    return -1;
+}
 
 
-// console.log(majorityElement(arr));
+console.log(majorityElement(arr));
 
 
 
 // *** Moore's voting algorithm ***
 
 
-// let arr2 = [1,2,2,5,5,1,5,1,4,5,5,5,4,5,5,5,5]
+let arr2 = [1,2,2,5,5,1,5,1,4,5,5,5,4,5,5,5,5]
 
-// const majorityElementMoore = function (arr){
-//     let cnt = 0;
-//     let element  = 0;
+const majorityElementMoore = function (arr){
+    let cnt = 0;
+    let element  = 0;
 
-//     for (let i = 0; i < arr.length; i++) {
-//         if (cnt == 0){
-//             cnt = 1;
-//             element = arr[i];
-//         }
-//         else if(arr[i] == element ){
-//             cnt++;
-//         }
-//         else{
-//             cnt--
-//         }
-//     }
+    for (let i = 0; i < arr.length; i++) {
+        if (cnt == 0){
+            cnt = 1;
+            element = arr[i];
+        }
+        else if(arr[i] == element ){
+            cnt++;
+        }
+        else{
+            cnt--
+        }
+    }
 
-//     let cnt1 = 0;
-//     for (let i = 0; i < arr.length; i++){
-//         if (arr[i] == element ) cnt1++;
-//     }
-//     if (cnt1 > Math.floor(arr.length/2)){
-//         return element;
-//     }
-//     return -1;
-// }
+    let cnt1 = 0;
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i] == element ) cnt1++;
+    }
+    if (cnt1 > Math.floor(arr.length/2)){
+        return element;
+    }
+    return -1;
+}
 
-// console.log(majorityElementMoore(arr2))
+console.log(majorityElementMoore(arr2))
 
 
 
@@ -84,7 +84,7 @@ const maxSubarraySum = function (arr){
     if(sum < 0 ){
         sum = 0;
     }
-    
+
     return maxi;
 }
 
