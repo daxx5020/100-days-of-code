c2bd497a6d410bdf90858aa3566cee0735b0520b
daxx5020 <dakshmakwana2146@gmail.com>
2024-06-13 22:49:21 +0530

    learned about pascal triangle and solved question

--- Day 19/PascalTriangle.js
+++ Day 19/PascalTriangle.js
@@ -1,3 +1,26 @@
 // Pascal Triangle
 
 
+const generateRow = function (row){
+    ans = 1;
+    ansRow = [];
+    ansRow.push(1);
+    for (let col = 1; col < row; col++){
+        ans = ans * (row-col);
+        ans = ans / (col);
+        ansRow.push(ans);
+    }
+    return ansRow;
+}
+
+
+const pascalTriangle = function (N){
+    let ans = [];
+    for (let i = 0; i <= N; i++){
+        ans.push(generateRow(i));
+    }
+
+    return ans;
+}
+
+console.log(pascalTriangle(5));
