c5e727d22dbf03a65912e0a59d278af00d64cc7b
daxx5020 <dakshmakwana2146@gmail.com>
2024-06-24 23:51:27 +0530

    learning about sorting of day 28

--- Day 28/InsertionSort.js
+++ Day 28/InsertionSort.js
@@ -1,8 +1,19 @@
 // Insertion sort
 
+let arr = [6,5,4,3,2,1]
 
 const insertionSort = function (arr){
+    for (let i = 0; i <= arr.length  - 1; i++) {
+        let j = i;
+        while(j > 0 && arr[j-1] > arr[j]){
+            let temp = arr[j-1];
+            arr[j-1] = arr[j];
+            arr[j] = temp;
+            j--;
+        }
 
+    }
+    return arr;
 }
 
 console.log(insertionSort(arr))
