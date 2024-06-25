791dbaf49f65589e02fc2d8b06c50bddffc5b15e
daxx5020 <dakshmakwana2146@gmail.com>
2024-06-25 22:51:17 +0530

    solved problems of day

--- Day 29/NextPermutation.js
+++ Day 29/NextPermutation.js
@@ -1,3 +1,45 @@
 // Next permutation
 
+let nums = [1,2,3]
+
+const nextGreaterPermutation = function(nums) {
+    let ind = -1;
+    const n = nums.length;
+
+    for (let i = n - 2; i >= 0; i--) {
+        if (nums[i] < nums[i + 1]) {
+            ind = i;
+            break;
+        }
+    }
+
+    if (ind === -1) {
+        nums.reverse();
+        return nums;
+    }
+
+    for (let i = n - 1; i > ind; i--) {
+        if (nums[i] > nums[ind]) {
+            [nums[i], nums[ind]] = [nums[ind], nums[i]];
+            break;
+        }
+    }
+
+    reverseArray(nums, ind + 1, n - 1);
+
+    return nums;
+}
+
+const reverseArray = function(arr, start, end) {
+    while (start < end) {
+        [arr[start], arr[end]] = [arr[end], arr[start]];
+        start++;
+        end--;
+    }
+}
+
+
+
+console.log(nextGreaterPermutation(nums));
+
  
