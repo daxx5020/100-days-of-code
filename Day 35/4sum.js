f31c0fcb3511bd23e3a03fa81c02a064494c8e54
daxx5020 <dakshmakwana2146@gmail.com>
2024-07-01 21:51:02 +0530

    solved 4 sum problem in optimal in day 35

--- Day 35/4sum.js
+++ Day 35/4sum.js
@@ -5,30 +5,30 @@ let target = 8;
 
 // *** Brute force Solution ***
 
-// const fourSumBruteForce = function(nums, target) {
-//     let n = nums.length;
-//     let set = new Set();
+const fourSumBruteForce = function(nums, target) {
+    let n = nums.length;
+    let set = new Set();
 
-//     for (let i = 0; i < n; i++) {
-//         for (let j = i + 1; j < n; j++) {
-//             for (let k = j + 1; k < n; k++) {
-//                 for (let l = k + 1; l < n; l++) {
-//                     let sum = nums[i] + nums[j] + nums[k] + nums[l];
+    for (let i = 0; i < n; i++) {
+        for (let j = i + 1; j < n; j++) {
+            for (let k = j + 1; k < n; k++) {
+                for (let l = k + 1; l < n; l++) {
+                    let sum = nums[i] + nums[j] + nums[k] + nums[l];
                     
-//                     if (sum === target) {
-//                         let temp = [nums[i], nums[j], nums[k], nums[l]].sort((a, b) => a - b);
-//                         set.add(temp.toString());
-//                     }
-//                 }
-//             }
-//         }
-//     }
+                    if (sum === target) {
+                        let temp = [nums[i], nums[j], nums[k], nums[l]].sort((a, b) => a - b);
+                        set.add(temp.toString());
+                    }
+                }
+            }
+        }
+    }
 
-//     let ans = Array.from(set).map(str => str.split(',').map(Number));
-//     return ans;
-// };
+    let ans = Array.from(set).map(str => str.split(',').map(Number));
+    return ans;
+};
 
-// console.log(fourSumBruteForce(nums, target));
+console.log(fourSumBruteForce(nums, target));
 
 
 
@@ -60,3 +60,38 @@ const fourSumBetter = function(nums,target){
 
 
 console.log(fourSumBetter(nums,target));
+
+
+
+// *** Optimal Solution ***
+
+
+const fourSumOptimal = function (nums,target){
+    let n = nums.length;
+    let ans = [];
+    nums.sort((a,b)=>a-b)
+
+    for(let i = 0; i < n; i++){
+        if (i > 0 && nums[i] === nums[i-1]) continue;
+        for (let j = i+1; j< n; j++){
+            if (j != (i+1) && nums[j] == nums[j-1]) continue;
+            let k = j+1;
+            let l = n-1;
+            while(k < l){
+                sum = nums[i] + nums[j] + nums[k] + nums[l];
+                if (sum === target){
+                    temp = [nums[i],nums[j],nums[k],nums[l]];
+                    ans.push(temp);
+                    while (k < l && nums[k] == nums[k-1]) k++;
+                    while (k < l && nums[l] == nums[l+1]) l--;
+                }
+                else if (sum < target) k++;
+                else l--;
+            }
+        }
+    }
+    return ans;
+}
+
+
+console.log(fourSumOptimal(nums,target))
