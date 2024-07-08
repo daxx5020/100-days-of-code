a3b0f6fe1f6e5adaf4f815f7f517c3794c7d2bc9
daxx5020 <dakshmakwana2146@gmail.com>
2024-07-08 12:11:26 +0530

    solved find floor question in day 41

--- Day 41/FloorInSortedArray.js
+++ Day 41/FloorInSortedArray.js
@@ -1,11 +1,49 @@
 // Floor in sorted array
 
-let n = 4
-let x = 4 
-let arr = [1,6,8,9];
+let n = 7
+let x = 5
+let arr = [1,2,8,10,11,12,19];
+
+
+/*
+    Bruteforce Solution
+*/
+
+const findFloorBruteForce = function (arr, n, x) {
+    let result = -1;
+
+    for (let i = 0; i < n; i++) {
+        if (arr[i] <= x) {
+            result = i;
+        }
+    }
+    return result;
+}
+
+
+console.log(findFloorBruteForce(arr,n,x));
+
+
+/*
+    Better Solution
+*/
 
 const sortedArray = function(arr,n,x){
+    let ans = -1;
+    let low = 0;
+    let high = n-1;
 
+    while (low <= high){
+        let mid = Math.floor((low + high) / 2);
+        if (arr[mid] <= x){
+            ans = mid;
+            low = mid + 1;
+        }
+        else{
+            high = mid -1;
+        }
+    }
+    return ans;
 }
 
 console.log(sortedArray(arr,n,x));
