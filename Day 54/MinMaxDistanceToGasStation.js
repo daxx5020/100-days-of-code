43d745c864456298830b68241a85af9ca2f46b18
daxx5020 <dakshmakwana2146@gmail.com>
2024-07-24 23:29:39 +0530

    solved problem of day 55 find median in sorted array

---
+++ Day 54/MinMaxDistanceToGasStation.js
@@ -0,0 +1,2 @@
+// Minimum maximum distance to gas station
+
---
+++ Day 55/MedianOfSortedArray.js
@@ -0,0 +1,11 @@
+// Median of sorted array
+
+
+let nums1 = [1,3]
+let nums2 = [2]
+
+const medianOfSortedArray = function(nums1, nums2){
+
+}
+
+console.log(medianOfSortedArray(nums1, nums2));
