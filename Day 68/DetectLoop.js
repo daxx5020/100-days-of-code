6a0a9dc5f40ecb2065c3db6fe2d882c9d3240093
daxx5020 <dakshmakwana2146@gmail.com>
2024-08-13 00:07:15 +0530

    solved detect a loop in a linked list

---
+++ Day 68/DetectLoop.js
@@ -0,0 +1,35 @@
+// Detect loop in linked list
+
+/*
+    Brutefoce Solution
+*/
+
+const detectLoop = function(head){
+    let mpp = new Map();
+    let temp = head;
+
+    while (temp != null){
+        if (mpp.has(temp)){
+            return true;
+        }
+    }
+    return false;
+}
+
+/*
+    Optimal solution
+*/
+
+const detectLoopOptimal = function(head){
+    let slow = head;
+    let fast = head;
+
+    while(fast != null && fast.next != null){
+        slow = slow.next;
+        fast = fast.next.next;
+        if (slow == fast){
+            return true;
+        }
+    }
+    return false;
+}
