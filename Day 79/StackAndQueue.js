3fe277de6934d720bb3b16237d2961b378c578b0
daxx5020 <dakshmakwana2146@gmail.com>
2024-08-24 01:18:39 +0530

    learning basic functions of the stack

--- Day 79/StackAndQueue.js
+++ Day 79/StackAndQueue.js
@@ -4,3 +4,58 @@
     Stack works with LIFO. (Last in first out)
     Queue works with FIFO. (First in first out)
 */
+
+
+class Stack {
+    constructor() {
+        this.top = -1;
+
+        this.size = 1000;
+
+        this.arr = new Array(this.size).fill(0);
+    }
+
+    push(x) {
+        // Increment the top pointer to indicate the new top position
+        this.top += 1;
+
+        // Place the new element at the top position in the array
+        this.arr[this.top] = x;
+    }
+
+    pop() {
+        
+        if (this.top === -1) {
+            return -1; // Return -1 if the stack is empty
+        }
+
+        // Retrieve the element at the top of the stack
+        let x = this.arr[this.top];
+
+        // Decrement the top pointer to remove the top element
+        this.top -= 1;
+
+        return x;
+    }
+
+    Top() {
+        return this.arr[this.top];
+    }
+
+    Size() {
+        return this.top + 1;
+    }
+}
+
+// Main execution
+const s = new Stack();
+s.push(6);
+s.push(3);
+s.push(7);
+
+console.log("Top of stack before deleting any element:", s.Top());
+console.log("Size of stack before deleting any element:", s.Size());
+
+console.log("The element deleted is:", s.pop());
+console.log("Size of stack after deleting an element:", s.Size());
+console.log("Top of stack after deleting an element:", s.Top());
