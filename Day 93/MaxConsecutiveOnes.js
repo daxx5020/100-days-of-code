5c39b566160adfb011c53387676d953bbe4290a4
daxx5020 <dakshmakwana2146@gmail.com>
2024-09-07 11:50:14 +0530

    solved max consecutive ones III problem

---
+++ Day 93/MaxConsecutiveOnes.js
@@ -0,0 +1,33 @@
+// Max consecutive ones III
+
+const longestOnes = function(nums,k){
+    let maxLen = 0;
+    let l = 0;
+    let r = 0;
+    let zeroes = 0;
+
+    while(r < nums.length){
+        if (nums[r] === 0){
+            zeroes++;
+        }
+
+        while(zeroes > k){
+            if(nums[l] === 0){
+                zeroes--;
+            }
+            l++;
+        }
+        
+        let len = r - l + 1;
+        maxLen = Math.max(maxLen, len);
+        
+        r++;
+    }
+    return maxLen;
+}
+
+
+let nums = [1,1,1,0,0,0,1,1,1,1,0];
+let k = 2;
+
+console.log(longestOnes(nums,k));
