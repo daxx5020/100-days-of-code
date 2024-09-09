61e4e26dc2a1a7219215d3539b09b89ad75a4145
daxx5020 <dakshmakwana2146@gmail.com>
2024-09-09 23:09:52 +0530

    solved maximum points you can obtain problem

--- Day 96/MaximumPointsYouCanObtain.js
+++ Day 96/MaximumPointsYouCanObtain.js
@@ -1,7 +1,24 @@
 // Maximum points you can obtain from cards
 
-const maxScore = function(carfPoints,k){
+const maxScore = function(cardPoints,k){
+    let lsum = 0;
+    let rsum = 0;
+    let maxScore = 0;
+    let n = cardPoints.length;
 
+    for (let i = 0; i < k; i++){
+        lsum = lsum + cardPoints[i];
+    }
+
+    maxScore = lsum;
+    
+    for (let i = 0; i < k; i++) {
+        lsum -= cardPoints[k - 1 - i];
+        rsum += cardPoints[n - 1 - i];
+
+        maxScore = Math.max(maxScore, lsum + rsum);
+    }
+    return maxScore;
 }
 
 let cardPoints = [1,2,3,4,5,6,1] 
